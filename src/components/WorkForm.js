cfd1e59534290f91cd8fe03d9573f8acc35ba136
Tom <87855471+Tom2612@users.noreply.github.com>
2022-07-24 21:51:10 +0800

    Add files via upload
    Started WorkForm.js

---
+++ src/components/WorkForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class WorkForm extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <h2>Hello</h2>
+        )
+    }
+}
+
+export default WorkForm;
