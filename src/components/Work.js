0fcee86d1a83bae64591943355f0947b87e9d8ef
Tom <87855471+Tom2612@users.noreply.github.com>
2022-07-24 21:50:45 +0800

    Update and rename work.js to Work.js
    Started Work display

---
+++ src/components/Work.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Work extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const { companyName, position, mainTasks, dateFrom, dateTo } = this.props;
+        return(
+            <div className="work--cv">
+                <h2>Company Name: {companyName}</h2>
+                <h2>Position: {position}</h2>
+                <h2>Main Tasks: {mainTasks}</h2>
+                <h2>Date From: {dateFrom}</h2>
+                <h2>Date To: {dateTo}</h2>
+                <button>Remove work</button>
+            </div>
+        )
+    }
+}
+
+export default Work;
