0597401ff866d8441006ba7144a3a1ee674e5342
Tom <87855471+Tom2612@users.noreply.github.com>
2022-07-26 10:41:14 +0800

    Update EducationForm.js
    Added form element and controls

--- src/components/EducationForm.js
+++ src/components/EducationForm.js
@@ -7,44 +7,53 @@ class EducationForm extends Component {
     render () {
         const { schoolName, degree, startYear, endYear } = this.props;
         return (
-            <div className='education--form--cv'>   
-                <label htmlFor='schoolName'>Enter your school name</label>
-                <input 
-                    type="text" 
-                    id="schoolName" 
-                    name="schoolName" 
-                    value={schoolName} 
-                    onChange={this.props.handleChangeEducation}
-                ></input>
-                <label htmlFor='titleOfStudy'>Enter your degree title</label>  
-                <input 
-                    type="text" 
-                    id="titleOfStudy" 
-                    name="degree" 
-                    value={degree} 
-                    onChange={this.props.handleChangeEducation}
-                ></input>
-                <label htmlFor='dateOfStudyStart'>When did you start?</label>
-                <input 
-                    type="month" 
-                    id="dateOfStudyStart" 
-                    min="1900-01" 
-                    max="2022-07" 
-                    name="startYear" 
-                    value={startYear} 
-                    onChange={this.props.handleChangeEducation}
-                ></input>
-                <label htmlFor='dateOfStudyEnd'>When did you end?</label>
-                <input 
-                    type="month" 
-                    id="dateOfStudyEnd" 
-                    min="1901-01"
-                    max="2022-07" 
-                    name="endYear" 
-                    value={endYear} 
-                    onChange={this.props.handleChangeEducation}
-                ></input>
-            </div>
+            <form onSubmit={this.props.onSubmitEducation} className="form education--form">
+                <div className='school-name'>
+                    <label htmlFor='schoolName'>Enter your school name</label>
+                    <input 
+                        type="text" 
+                        id="schoolName" 
+                        name="schoolName" 
+                        value={schoolName} 
+                        onChange={this.props.handleChangeEducation}
+                    ></input>
+                </div>  
+                <div className='degree-name'>
+                    <label htmlFor='titleOfStudy'>Enter your degree title</label>  
+                    <input 
+                        type="text" 
+                        id="titleOfStudy" 
+                        name="degree" 
+                        value={degree} 
+                        onChange={this.props.handleChangeEducation}
+                    ></input>
+                </div>  
+                <div className='year-start'>
+                    <label htmlFor='dateOfStudyStart'>When did you start?</label>
+                    <input 
+                        type="month" 
+                        id="dateOfStudyStart" 
+                        min="1900-01" 
+                        max="2022-07" 
+                        name="startYear" 
+                        value={startYear} 
+                        onChange={this.props.handleChangeEducation}
+                    ></input>
+                </div>  
+                <div className='year-end'>
+                    <label htmlFor='dateOfStudyEnd'>When did you end?</label>
+                    <input 
+                        type="month" 
+                        id="dateOfStudyEnd" 
+                        min="1901-01"
+                        max="2022-07" 
+                        name="endYear" 
+                        value={endYear} 
+                        onChange={this.props.handleChangeEducation}
+                    ></input>
+                </div>  
+                <button className="submit-btn" type="submit">Submit section</button>
+            </form>
         )
     }
 }
