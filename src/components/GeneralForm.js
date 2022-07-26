887c46a8694fd9782e0967423e94732e933204f5
Tom <87855471+Tom2612@users.noreply.github.com>
2022-07-26 10:40:55 +0800

    Update GeneralForm.js
    Added form element and controls

--- src/components/GeneralForm.js
+++ src/components/GeneralForm.js
@@ -7,32 +7,39 @@ class General extends Component {
     render() {
         const { name, email, phone } = this.props;
         return (
-            <div className="general--form--input">   
-                <label htmlFor='name'>Enter your name</label>
-                <input 
-                    type="text" 
-                    id="name" 
-                    name="name" 
-                    value={name} 
-                    onChange={this.props.handleChangeGeneral}
-                ></input>
-                <label htmlFor='email'>Enter your email</label>  
-                <input 
-                    type="email" 
-                    id="email" 
-                    name="email" 
-                    value={email} 
-                    onChange={this.props.handleChangeGeneral}
-                ></input>
-                <label htmlFor='phone'>Enter your phone number</label>
-                <input 
-                    type="text" 
-                    id="number" 
-                    name="phone" 
-                    value={phone} 
-                    onChange={this.props.handleChangeGeneral}
-                ></input>
-            </div>
+            <form onSubmit={this.props.onSubmitGeneral} className="form general--form">
+                <div className='name-input'>
+                    <label htmlFor='name'>Full name:</label>
+                    <input 
+                        type="text" 
+                        id="name" 
+                        name="name" 
+                        value={name} 
+                        onChange={this.props.handleChangeGeneral}
+                    ></input>
+                </div>
+                <div className='email-input'>
+                    <label htmlFor='email'>Email address:</label>  
+                    <input 
+                        type="email" 
+                        id="email" 
+                        name="email" 
+                        value={email} 
+                        onChange={this.props.handleChangeGeneral}
+                    ></input>
+                </div>
+                <div className='phone-input'>
+                    <label htmlFor='phone'>Contact number:</label>
+                    <input 
+                        type="text" 
+                        id="number" 
+                        name="phone" 
+                        value={phone} 
+                        onChange={this.props.handleChangeGeneral}
+                    ></input>
+                </div>
+                <button className='submit-btn' type="submit">Submit section</button>
+            </form>
         )
     }
 }
