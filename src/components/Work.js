ec8b8d479cef9949e32a7f33c77a3f270c1afc8e
Tom <87855471+Tom2612@users.noreply.github.com>
2022-07-26 17:32:12 +0800

    Update Work.js
    Updated, starting to add map functionality

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,16 +5,23 @@ class Work extends Component {
         super(props);
     }
     render() {
-        const { companyName, position, mainTasks, dateFrom, dateTo } = this.props;
+        // const { companyName, position, mainTasks, dateFrom, dateTo } = this.props;
+        const { allWork } = this.props;
         return(
-            <div className="work--cv">
-                <h2>Company Name: {companyName}</h2>
-                <h2>Position: {position}</h2>
-                <h2>Main Tasks: {mainTasks}</h2>
-                <h2>Date From: {dateFrom}</h2>
-                <h2>Date To: {dateTo}</h2>
-                <button>Remove work</button>
+            <div>
+                { allWork.map((work) => {
+                return <p>{work.companyName}</p>;
+            })}
             </div>
+            
+            // <div className="work--cv">
+            //     <h2>Company Name: {companyName}</h2>
+            //     <h2>Position: {position}</h2>
+            //     <h2>Main Tasks: {mainTasks}</h2>
+            //     <h2>Date From: {dateFrom}</h2>
+            //     <h2>Date To: {dateTo}</h2>
+            //     <button>Remove work</button>
+            // </div>
         )
     }
 }
