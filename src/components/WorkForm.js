bb41030c9b5dd768c722ba397bcd8915c8f3b6bd
Tom <87855471+Tom2612@users.noreply.github.com>
2022-07-26 17:30:37 +0800

    Update WorkForm.js
    Added workForm

--- src/components/WorkForm.js
+++ src/components/WorkForm.js
@@ -5,8 +5,68 @@ class WorkForm extends Component {
         super(props)
     }
     render() {
+        const { companyName, position, mainTasks, dateFrom, dateTo } = this.props;
         return (
-            <h2>Hello</h2>
+            <form onSubmit={this.props.onSubmitWork} className="form work--form">
+                <div className='form-title'>
+                    <h2>Add previous work information here:</h2>
+                </div>
+                <div className="companyName">
+                    <label>Company name</label>
+                    <input 
+                        type="text"
+                        id="companyName"
+                        name="companyName"
+                        value={companyName}
+                        onChange={this.props.handleChangeWork}
+                    ></input>
+                </div>
+                <div className='position'>
+                    <label htmlFor='position'>Enter your position title</label>
+                    <input 
+                        type="text" 
+                        id="position" 
+                        name="position" 
+                        value={position}
+                        onChange={this.props.handleChangeWork}
+                    ></input>
+                </div>
+                <div className='main-tasks'>
+                    <label htmlFor='mainTasks'>Enter your main tasks</label>
+                    <textarea 
+                        id="mainTasks" 
+                        name="mainTasks"
+                        cols="20"
+                        value={mainTasks}
+                        onChange={this.props.handleChangeWork}
+                    ></textarea>
+                </div>
+                <div className='dateFrom'>
+                    <label htmlFor='dateFrom'>When did you start?</label>
+                    <input 
+                        type="month" 
+                        min="1900-01" 
+                        max="2022-07"  
+                        id="dateFrom" 
+                        name="dateFrom" 
+                        value={dateFrom}
+                        onChange={this.props.handleChangeWork}
+                    ></input>
+                </div>
+                <div className='dateTo'>
+                    <label htmlFor='dateTo'>When did you finish?</label>
+                    <input 
+                        type="month" 
+                        min="1900-01" 
+                        max="2022-07" 
+                        id="dateTo" 
+                        name="dateTo" 
+                        value={dateTo}
+                        onChange={this.props.handleChangeWork}
+                    ></input>
+                </div>
+                <button className="submit-btn" type="submit">Submit section</button>
+            </form>
         )
     }
 }
