9b87f33ff60777d061885b877da10458e6e619d1
Tom <87855471+Tom2612@users.noreply.github.com>
2022-07-27 21:12:45 +0800

    Update Work.js
    Changed for styling purposes

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,20 +8,22 @@ class Work extends Component {
         // const { companyName, position, mainTasks, dateFrom, dateTo } = this.props;
         const { allWork } = this.props;
         return(
-            <div>
+            <div className='cv work--cv'>
+                <h2 className='section-title'>Career Summary</h2>
                 { allWork.map((work) => {
-                return <p>{work.companyName}</p>;
+                return (
+                    <div className="work--item" key={work.companyName}>
+                        <h2>{work.position} - {work.companyName}</h2>
+                        {/* <h2>Company Name: {work.companyName}</h2>
+                        <h2>Position Held: {work.position}</h2> */}
+                        <p>Main Tasks: {work.mainTasks}</p>
+                        <h3>Date from: {work.dateFrom}</h3>
+                        <h3>Date To: {work.dateTo}</h3>
+                    </div>
+                )
             })}
+            <button onClick={this.props.onSubmitWork} className="edit-btn"><span>Edit</span></button>
             </div>
-            
-            // <div className="work--cv">
-            //     <h2>Company Name: {companyName}</h2>
-            //     <h2>Position: {position}</h2>
-            //     <h2>Main Tasks: {mainTasks}</h2>
-            //     <h2>Date From: {dateFrom}</h2>
-            //     <h2>Date To: {dateTo}</h2>
-            //     <button>Remove work</button>
-            // </div>
         )
     }
 }
