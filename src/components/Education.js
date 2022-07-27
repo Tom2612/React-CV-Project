4be8cd5f53c97651232132ec659200929c4ddb99
Tom <87855471+Tom2612@users.noreply.github.com>
2022-07-27 21:12:20 +0800

    Update Education.js
    Changed for styling purposes

--- src/components/Education.js
+++ src/components/Education.js
@@ -7,12 +7,16 @@ class Education extends Component {
     render() {
         const { schoolName, degree, startYear, endYear } = this.props;
         return(
-            <div className="education--cv">
-                <h2>Institute: {schoolName}</h2>
-                <h2>degree: {degree}</h2>
-                <h2>Start Year: {startYear}</h2>
-                <h2>End Year: {endYear}</h2>
-                <button onClick={this.props.onSubmitEducation}>Edit</button>
+            <div className="cv education--cv">
+                <h2 className='section-title'>Education</h2>
+                <div className='information'>
+                    <h3><strong>{startYear.slice(0, 4)} - {endYear.slice(0, 4)}:</strong> {schoolName} | {degree}</h3>
+                    {/* <h3>Institute: {schoolName}</h3>
+                    <h3>Degree: {degree}</h3>
+                    <h3>Start Year: {startYear}</h3>
+                    <h3>End Year: {endYear}</h3> */}
+                </div>
+                <button onClick={this.props.onSubmitEducation} className="edit-btn"><span>Edit</span></button>
             </div>
         )
     }
