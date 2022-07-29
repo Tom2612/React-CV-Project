13ffded5173299f0d96050b672f794d6db09d8ee
Tom <87855471+Tom2612@users.noreply.github.com>
2022-07-29 12:08:25 +0800

    Update Education.js
    Removed old code

--- src/components/Education.js
+++ src/components/Education.js
@@ -11,10 +11,6 @@ class Education extends Component {
                 <h2 className='section-title'>Education</h2>
                 <div className='information'>
                     <h3><strong>{startYear.slice(0, 4)} - {endYear.slice(0, 4)}:</strong> {schoolName} | {degree}</h3>
-                    {/* <h3>Institute: {schoolName}</h3>
-                    <h3>Degree: {degree}</h3>
-                    <h3>Start Year: {startYear}</h3>
-                    <h3>End Year: {endYear}</h3> */}
                 </div>
                 <button onClick={this.props.onSubmitEducation} className="edit-btn"><span>Edit</span></button>
             </div>
